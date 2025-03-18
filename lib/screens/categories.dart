793226bc055fa975112861896d3d81d80b3c9ed7
Wealth1000 <anthonyaddowealth@gmail.com>
2025-03-18 12:39:48 +0000

    Making Progress

---
+++ lib/screens/categories.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:learnflutter/data/dummy_data.dart';
+import 'package:learnflutter/models/category.dart';
+import 'package:learnflutter/models/meal.dart';
+import 'package:learnflutter/screens/meals.dart';
+import 'package:learnflutter/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key, required this.onToggleFavorite});
+  final void Function(Meal meal) onToggleFavorite;
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals =
+        dummyMeals
+            .where((meal) => meal.categories.contains(category.id))
+            .toList();
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder:
+            (ctx) => MealsScreen(
+              title: category.title,
+              meals: filteredMeals,
+              onToggleFavorite: onToggleFavorite,
+            ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: const EdgeInsets.all(24),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: [
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          ),
+      ],
+    );
+  }
+}
---
+++ lib/screens/tabs.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:learnflutter/models/meal.dart';
+import 'package:learnflutter/screens/categories.dart';
+import 'package:learnflutter/screens/meals.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+  final List<Meal> _favoriteMeals = [];
+  void _toggleMealFavoriteStatus(Meal meal) {
+    final isExisting = _favoriteMeals.contains(meal);
+    isExisting ? _favoriteMeals.remove(meal) : _favoriteMeals.add(meal);
+  }
+
+  void selectPage(index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = CategoriesScreen(onToggleFavorite: _toggleMealFavoriteStatus,);
+    var activePageTitle = "Categories";
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(
+        meals: [],
+        onToggleFavorite: _toggleMealFavoriteStatus,
+      );
+      activePageTitle = "Your Favorites";
+    }
+    return Scaffold(
+      appBar: AppBar(title: Text(activePageTitle)),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favorites"),
+        ],
+      ),
+    );
+  }
+}
